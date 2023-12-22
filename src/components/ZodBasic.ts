497ae269067fbb62e39a6b3fd954e8f1ed72c4e0
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-22 23:28:44 +0530

    FEAT : ZOD TYPES
    FEAT : ZOD TYPES for items

--- src/components/ZodBasic.ts
+++ src/components/ZodBasic.ts
@@ -1,13 +1,26 @@
 import { z } from "zod";
 
-const UserSchema = z.object({
-  username: z.string(),
-});
+ const hobbies = ["Programming", "Weigh Lifting", "Guitar"] as const
 
+const UserSchema = z.object({
+  username: z.string().min(3),
+  age: z.number().default(Math.random),
+  birthday: z.date(),
+  isProgrammer: z.literal(true),
+  // hobby: z.enum(["Programming", "Weigh Lifting", "Guitar"]),
+  hobby: z.enum(hobbies),
+});
 
 // extract the inferred type
 type User = z.infer<typeof UserSchema>;
 
-const user: User = {username: "SSG"}
+const user: User = {
+  username: "SSG",
+  age: 55,
+  birthday: new Date(),
+  isProgrammer: true,
+  hobby: "Programming",
+};
 
-console.log(UserSchema.parse(user))
+console.log(UserSchema.parse(user));
+// console.log(UserSchema.safeParse(user).success);
