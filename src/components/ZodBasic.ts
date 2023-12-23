d6e9219270c7d958982fd58238faabfcd2636a48
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-23 16:43:27 +0530

    learn : Zod advance type
    learn : Zod advance type

--- src/components/ZodBasic.ts
+++ src/components/ZodBasic.ts
@@ -2,21 +2,27 @@ import { z } from "zod";
 
 const hobbies = ["Programming", "Weigh Lifting", "Guitar"] as const;
 
-const UserSchema = z
-  .object({
-    username: z.string().min(3),
-    age: z.number().default(Math.random),
-    birthday: z.date(),
-    isProgrammer: z.literal(true),
-    // hobby: z.enum(["Programming", "Weigh Lifting", "Guitar"]),
-    hobby: z.enum(hobbies),
-    friends: z.array(z.string())
-    // .nonempty()
-  })
-  // .strict()
-  // .passthrough()
-  // .merge(z.object({ name: z.string()}))
-  // .extend({ name: z.string() });
+const UserSchema = z.object({
+  uid: z.discriminatedUnion("status", [
+    z.object({status: z.literal("success"), date: z.string()}),
+    z.object({status: z.literal("failed"), date: z.instanceof(Error)})
+  ]),
+  id: z.string().or(z.number()),
+  // id: z.union([z.string(), z.number()]),
+  username: z.string().min(3),
+  age: z.number().default(Math.random),
+  birthday: z.date(),
+  isProgrammer: z.literal(true),
+  // hobby: z.enum(["Programming", "Weigh Lifting", "Guitar"]),
+  hobby: z.enum(hobbies),
+  friends: z.array(z.string()),
+  // .nonempty()
+  coords: z.tuple([z.string(), z.date()]).rest(z.number()),
+});
+// .strict()
+// .passthrough()
+// .merge(z.object({ name: z.string()}))
+// .extend({ name: z.string() });
 // .deepPartial()
 // .omit({age: true})
 // .pick({username: true})
@@ -25,6 +31,8 @@ const UserSchema = z
 type User = z.infer<typeof UserSchema>;
 
 const user: User = {
+  uid: {status: "success" ,date: "fdgdstdh"} , 
+  id: "kkjhnhj",
   username: "SSG",
   age: 0,
   birthday: new Date(),
@@ -33,6 +41,7 @@ const user: User = {
   // name: "",
   // bangla: "santa"
   friends: ["bala", "7", "MM"],
+  coords: ["test", new Date(), 4, 4, 4, 44, 446],
 };
 
 console.log(UserSchema.partial().parse(user));
