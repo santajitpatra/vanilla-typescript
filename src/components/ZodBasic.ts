3450abb575d60a8af8451a29f41f4aadce7b6fa7
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-26 16:50:31 +0530

    learn zod PromiseSchema
    learn zod PromiseSchema

--- src/components/ZodBasic.ts
+++ src/components/ZodBasic.ts
@@ -9,7 +9,7 @@ const UserSchema = z.object({
   ]),
   id: z.string().or(z.number()),
   // id: z.union([z.string(), z.number()]),
-  username: z.string().min(3),
+  username: z.string().min(3, "min lenth must be 3"),
   age: z.number().default(Math.random),
   birthday: z.date(),
   isProgrammer: z.literal(true),
@@ -44,7 +44,7 @@ const user: User = {
   coords: ["test", new Date(), 4, 4, 4, 44, 446],
 };
 
-console.log(UserSchema.partial().parse(user));
+console.log(UserSchema.partial().safeParse(user));
 // console.log(UserSchema.safeParse(user).success);
 
 
@@ -78,3 +78,22 @@ const UserSet = z.set(z.number());
 const userMapingDataSet = new Set([1,1,1,1,6367,566,65,565])
 
 console.log(UserSet.parse(userMapingDataSet))
+
+// 5th Lesson
+const PromiseSchema = z.promise(z.string());
+
+const p = Promise.resolve("dsfsdf")
+
+console.log(PromiseSchema.parse(p))
+
+// 6th Lesson
+const brandEmail = z
+  .string()
+  .email()
+  .refine((val) => val.endsWith("@santajitpatra.com"), {
+    message: "Email must end with @santajitpatra.com",
+  });
+
+const email = "one@santajitpatra.com";
+
+console.log(brandEmail.parse(email))
